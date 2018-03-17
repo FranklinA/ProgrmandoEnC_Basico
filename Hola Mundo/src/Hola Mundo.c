45ef4d1243b9ea3b1b387fc10faea6fdebc6109f
FranklinA <angulofranklin@hotmail.com>
2018-03-17 15:22:19 -0300

    Leccion 1
    Lec 1

---
+++ Hola Mundo/src/Hola Mundo.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Hola.c
+ Author      : Frank
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>/* Libreria Pedir y guardar datos*/
+#include <stdlib.h>
+
+int main(void) {/*Funcion principal*/
+	printf("Hola mundo :D \n"); /*Imprimes en pantalla*/
+	return 0;
+}
